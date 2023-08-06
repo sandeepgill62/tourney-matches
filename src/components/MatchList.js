fdfd2fa12003f114295f9ca6374985e2f34bff9b
Sandeep Gill <sandeepgill62@gmail.com>
2023-08-06 05:27:00 +0000

    Passing Data Down with Props

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -5,10 +5,12 @@ import matchData from '../data/matchData';
 import Match from "./Match";
 
 function MatchList (props) {
+  const oneMatch = matchData[0];
+
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match />
+      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   );
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,22 +1,21 @@
 // PlayerList.js
 
 import React from 'react'
+import Player from "./Player";
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
-import Player from "./Player";
-
 
 function PlayerList (props) {
 
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-  console.log(parsedPlayerData);
+  const onePlayer = parsedPlayerData[0];
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
 }
